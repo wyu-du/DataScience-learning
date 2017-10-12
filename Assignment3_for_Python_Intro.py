6876661594c6822ecf32946775777ebcc3a8519b
think <310445479@qq.com>
2017-10-12 16:38:11 +0800

    modify the display format

--- Assignment3_for_Python_Intro.py
+++ Assignment3_for_Python_Intro.py
@@ -1,53 +1,5 @@
 # coding: utf-8
 
-
-# # Assignment 3 - More Pandas
-# This assignment requires more individual learning then the last one did - you are encouraged to check out the [pandas documentation](http://pandas.pydata.org/pandas-docs/stable/) to find functions or methods you might not have used yet, or ask questions on [Stack Overflow](http://stackoverflow.com/) and tag them as pandas and python related. And of course, the discussion forums are open for interaction with your peers and the course staff.
-
-# ### Question 1 (20%)
-# Load the energy data from the file `Energy Indicators.xls`, which is a list of indicators of [energy supply and renewable electricity production](Energy%20Indicators.xls) from the [United Nations](http://unstats.un.org/unsd/environment/excel_file_tables/2013/Energy%20Indicators.xls) for the year 2013, and should be put into a DataFrame with the variable name of **energy**.
-# 
-# Keep in mind that this is an Excel file, and not a comma separated values file. Also, make sure to exclude the footer and header information from the datafile. The first two columns are unneccessary, so you should get rid of them, and you should change the column labels so that the columns are:
-# 
-# `['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']`
-# 
-# Convert `Energy Supply` to gigajoules (there are 1,000,000 gigajoules in a petajoule). For all countries which have missing data (e.g. data with "...") make sure this is reflected as `np.NaN` values.
-# 
-# Rename the following list of countries (for use in later questions):
-# 
-# ```"Republic of Korea": "South Korea",
-# "United States of America": "United States",
-# "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
-# "China, Hong Kong Special Administrative Region": "Hong Kong"```
-# 
-# There are also several countries with numbers and/or parenthesis in their name. Be sure to remove these, 
-# 
-# e.g. 
-# 
-# `'Bolivia (Plurinational State of)'` should be `'Bolivia'`, 
-# 
-# `'Switzerland17'` should be `'Switzerland'`.
-# 
-# 
-# Next, load the GDP data from the file `world_bank.csv`, which is a csv containing countries' GDP from 1960 to 2015 from [World Bank](http://data.worldbank.org/indicator/NY.GDP.MKTP.CD). Call this DataFrame **GDP**. 
-# 
-# Make sure to skip the header, and rename the following list of countries:
-# 
-# ```"Korea, Rep.": "South Korea", 
-# "Iran, Islamic Rep.": "Iran",
-# "Hong Kong SAR, China": "Hong Kong"```
-# 
-# 
-# Finally, load the [Sciamgo Journal and Country Rank data for Energy Engineering and Power Technology](http://www.scimagojr.com/countryrank.php?category=2102) from the file `scimagojr-3.xlsx`, which ranks countries based on their journal contributions in the aforementioned area. Call this DataFrame **ScimEn**.
-# 
-# Join the three datasets: GDP, Energy, and ScimEn into a new dataset (using the intersection of country names). Use only the last 10 years (2006-2015) of GDP data and only the top 15 countries by Scimagojr 'Rank' (Rank 1 through 15). 
-# 
-# The index of this DataFrame should be the name of the country, and the columns should be ['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations',
-#        'Citations per document', 'H index', 'Energy Supply',
-#        'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008',
-#        '2009', '2010', '2011', '2012', '2013', '2014', '2015'].
-
-
 import pandas as pd
 import numpy as np 
 import re
@@ -65,7 +17,6 @@ def convert_country(row):
     if row['Country']=="China, Hong Kong Special Administrative Region": 
         row['Country']="Hong Kong"
     return row['Country']
-
 def rename_country(row):
     if row['Country Name']=="Korea, Rep.":
         row['Country Name']="South Korea"
@@ -74,7 +25,6 @@ def rename_country(row):
     if row['Country Name']=="Hong Kong SAR, China": 
         row['Country Name']="Hong Kong"
     return row['Country Name']
-
 def answer_one():
     energy=pd.read_excel('Energy Indicators.xls', skiprows=17, skip_footer=38, parse_cols=[2,3,4,5], 
                          names=['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable'],
@@ -96,13 +46,7 @@ def answer_one():
     output[['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'H index']]=output[['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'H index']].astype('Int64')
     return output
 
-answer_one()
-
-
-# ### Question 2 (6.6%)
-# The previous question joined three datasets then reduced this to just the top 15 entries. When you joined the datasets, but before you reduced this to the top 15 items, how many entries did you lose?
-
-
+	
 def answer_two():
     #items lost = len(union of all three df) - len(intersection of all 3 df)
     energy=pd.read_excel('Energy Indicators.xls', skiprows=17, skip_footer=38, parse_cols=[2,3,4,5], 
@@ -124,55 +68,30 @@ def answer_two():
     output=len(t2)-len(t4)
     return output
 
-answer_two()
-
-
-# ### Question 3 (6.6%)
-# What is the average GDP over the last 10 years for each country? (exclude missing values from this calculation.)
-#
-
+	
 def avgGDP(row):
     data=row[['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']]
     row['avgGDP']=np.mean(data)
     return row
-
 def answer_three():
     Top15 = answer_one()
     output=Top15.apply(avgGDP, axis=1)
     output=output.sort_values(by='avgGDP',ascending=False)
     return output['avgGDP']
 
-answer_three()
-
-
-# ### Question 4 (6.6%)
-# By how much had the GDP changed over the 10 year span for the country with the 6th largest average GDP?
-# 
-
+	
 def answer_four():
     Top15 = answer_one()
     output=Top15.loc['United Kingdom','2015']-Top15.loc['United Kingdom','2006']
     return output
 
-answer_four()
-
-
-# ### Question 5 (6.6%)
-# What is the mean `Energy Supply per Capita`?
-# 
-
+	
 def answer_five():
     Top15 = answer_one()
     output=np.mean(Top15['Energy Supply per Capita'])
     return output
 
-answer_five()
-
-
-# ### Question 6 (6.6%)
-# What country has the maximum % Renewable and what is the percentage?
-# 
-
+	
 def answer_six():
     Top15 = answer_one()
     country_list=Top15.sort_values(by='% Renewable',ascending=False)
@@ -181,18 +100,10 @@ def answer_six():
     output=(country,percentage)
     return output
 
-answer_six()
-
-
-# ### Question 7 (6.6%)
-# Create a new column that is the ratio of Self-Citations to Total Citations. 
-# What is the maximum value for this new column, and what country has the highest ratio?
-# 
-
+	
 def ratio(row):
     row['ratio']=row['Self-citations']/row['Citations']
     return row
-
 def answer_seven():
     Top15 = answer_one()
     Top15=Top15.apply(ratio, axis=1)
@@ -202,37 +113,20 @@ def answer_seven():
     output=(country,value)
     return output
 
-answer_seven()
-
-
-# ### Question 8 (6.6%)
-# 
-# Create a column that estimates the population using Energy Supply and Energy Supply per capita. 
-# What is the third most populous country according to this estimate?
-# 
-
+	
 def population(row):
     row['population']=row['Energy Supply']/row['Energy Supply per Capita']
     return row
-
 def answer_eight():
     Top15 = answer_one()
     Top15=Top15.apply(population, axis=1).sort_values(by='population',ascending=False)
     country=Top15.index[2]
     return country
 
-answer_eight()
-
-
-# ### Question 9 (6.6%)
-# Create a column that estimates the number of citable documents per person. 
-# What is the correlation between the number of citable documents per capita and the energy supply per capita? Use the `.corr()` method, (Pearson's correlation).
-#  
-
+	
 def DC_per(row):
     row['Citable documents per capita']=row['Citable documents']/row['population']
     return row
-
 def answer_nine():
     Top15 = answer_one()
     Top15=Top15.apply(population, axis=1)
@@ -241,52 +135,21 @@ def answer_nine():
     output=corr.loc['Citable documents per capita','Energy Supply per Capita']
     return output
 
-answer_nine()
-
-
-# ### Question 10 (6.6%)
-# Create a new column with a 1 if the country's % Renewable value is at or above the median for all countries in the top 15, and a 0 if the country's % Renewable value is below the median.
-# 
-
+	
 def discrimintate(data, median):
     if data<median:
         data=0
     else:
         data=1
     return data
-
 def answer_ten():
     Top15 = answer_one()
     median=Top15['% Renewable'].median()
     Top15['HighRenew']=Top15.apply(lambda x: discrimintate(x['% Renewable'],median), axis=1)
     HighRenew=Top15.sort_values(by='Rank', ascending=True)['HighRenew']
     return HighRenew
-
-answer_ten()
-
-
-# ### Question 11 (6.6%)
-# Use the following dictionary to group the Countries by Continent, then create a dateframe that displays the sample size (the number of countries in each continent bin), and the sum, mean, and std deviation for the estimated population of each country.
-# 
-# ```python
-# ContinentDict  = {'China':'Asia', 
-#                   'United States':'North America', 
-#                   'Japan':'Asia', 
-#                   'United Kingdom':'Europe', 
-#                   'Russian Federation':'Europe', 
-#                   'Canada':'North America', 
-#                   'Germany':'Europe', 
-#                   'India':'Asia',
-#                   'France':'Europe', 
-#                   'South Korea':'Asia', 
-#                   'Italy':'Europe', 
-#                   'Spain':'Europe', 
-#                   'Iran':'Asia',
-#                   'Australia':'Australia', 
-#                   'Brazil':'South America'}
-# ```
-# 
-
+	
+	
 def groupBycontinent(row):
     ContinentDict  = {'China':'Asia', 
                       'United States':'North America', 
@@ -305,7 +168,6 @@ def groupBycontinent(row):
                       'Brazil':'South America'}
     row['Continent']=ContinentDict[row[0]]
     return row
-
 def answer_eleven():
     Top15 = answer_one()
     Top15=Top15.reset_index()
@@ -319,13 +181,7 @@ def answer_eleven():
         df.loc[continent,'std']=np.std(frame['population'])
     return df.astype('float64')
 
-answer_eleven()
-
-
-# ### Question 12 (6.6%)
-# Cut % Renewable into 5 bins. Group Top15 by the Continent, as well as these new % Renewable bins. How many countries are in each of these groups?
-# 
-
+	
 def answer_twelve():
     Top15 = answer_one()
     Top15=Top15.reset_index()
@@ -338,23 +194,12 @@ def answer_twelve():
     output=Top15.groupby(['Continent','Bins'])['Continent'].agg(len)
     return output
 
-answer_twelve()
-
-
-# ### Question 13 (6.6%)
-# Convert the Population Estimate series to a string with thousands separator (using commas). Do not round the results.
-# 
-# e.g. 317615384.61538464 -> 317,615,384.61538464
-# 
-
+	
 def transtype(row):
     row['PopEst']=format(row['population'],',')
     return row
-
 def answer_thirteen():
     Top15 = answer_one()
     Top15=Top15.apply(population, axis=1)
     output=Top15.apply(transtype, axis=1)['PopEst']
     return output
-
-answer_thirteen()
--- Assignment4_for_Python_Intro.py
+++ Assignment4_for_Python_Intro.py
@@ -1,18 +1,5 @@
 # coding: utf-8
 
-
-# # Assignment 4 - Hypothesis Testing
-# This assignment requires more individual learning than previous assignments - you are encouraged to check out the [pandas documentation](http://pandas.pydata.org/pandas-docs/stable/) to find functions or methods you might not have used yet, or ask questions on [Stack Overflow](http://stackoverflow.com/) and tag them as pandas and python related. And of course, the discussion forums are open for interaction with your peers and the course staff.
-# 
-# Definitions:
-# * A _quarter_ is a specific three month period, Q1 is January through March, Q2 is April through June, Q3 is July through September, Q4 is October through December.
-# * A _recession_ is defined as starting with two consecutive quarters of GDP decline, and ending with two consecutive quarters of GDP growth.
-# * A _recession bottom_ is the quarter within a recession which had the lowest GDP.
-# * A _university town_ is a city which has a high percentage of university students compared to the total population of the city.
-# 
-# **Hypothesis**: University towns have their mean housing prices less effected by recessions. Run a t-test to compare the ratio of the mean price of houses in university towns the quarter before the recession starts compared to the recession bottom. (`price_ratio=quarter_before_recession/recession_bottom`)
-# 
-
 import pandas as pd
 import numpy as np
 from scipy.stats import ttest_ind
@@ -20,19 +7,7 @@ from scipy.stats import ttest_ind
 # Use this dictionary to map state names to two letter acronyms
 states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
 
-
 def get_list_of_university_towns():
-    '''Returns a DataFrame of towns and the states they are in from the 
-    university_towns.txt list. The format of the DataFrame should be:
-    DataFrame( [ ["Michigan", "Ann Arbor"], ["Michigan", "Yipsilanti"] ], 
-    columns=["State", "RegionName"]  )
-    
-    The following cleaning needs to be done:
-
-    1. For "State", removing characters from "[" to the end.
-    2. For "RegionName", when applicable, removing every character from " (" to the end.
-    3. Depending on how you read the data, you may need to remove newline character '\n'. '''
-    
     f=open('university_towns.txt')
     lines=f.readlines()
     l=[]
@@ -45,12 +20,8 @@ def get_list_of_university_towns():
     df=pd.DataFrame(l,columns=["State", "RegionName"])
     return df
 
-get_list_of_university_towns()
-
-
 
 def get_GDP():
-    '''Returns a dataframe with columns=["time", "GDP", "trend"]'''
     df=pd.read_excel('gdplev.xls', parse_cols=[4, 6], skiprows=219, names=['time','GDP'])
     out=[]
     for i in range(len(df)):
@@ -67,9 +38,8 @@ def get_GDP():
     df['trend']=out
     return df
 
+	
 def get_recession_start():
-    '''Returns the year and quarter of the recession start time as a 
-    string value in a format such as 2005q3'''
     df=get_GDP()
     flag=0
     for i in range(1,len(df)-4):
@@ -83,11 +53,8 @@ def get_recession_start():
             pos=i-flag+1
     return df.iloc[pos,0]
 
-get_recession_start()
-
+	
 def get_recession_end():
-    '''Returns the year and quarter of the recession end time as a 
-    string value in a format such as 2005q3'''
     df=get_GDP()
     for i in range(1,len(df)-4):
         target=df.iloc[i:i+4,2]
@@ -96,11 +63,8 @@ def get_recession_end():
             out=df.iloc[i+3,0]
     return out
 
-get_recession_end()
 
 def get_recession_bottom():
-    '''Returns the year and quarter of the recession bottom time as a 
-    string value in a format such as 2005q3'''
     df=get_GDP()
     for i in range(1,len(df)-4):
         target=df.iloc[i:i+4,2]
@@ -109,8 +73,7 @@ def get_recession_bottom():
             out=df.iloc[i+1,0]
     return out
 
-get_recession_bottom()
-
+	
 def quarter(row):
     index=str(row['index'])
     if index[5:]=='03':
@@ -124,17 +87,6 @@ def quarter(row):
     return index
 
 def convert_housing_data_to_quarters():
-    '''
-    Converts the housing data to quarters and returns it as mean 
-    values in a dataframe. This dataframe should be a dataframe with
-    columns for 2000q1 through 2016q3, and should have a multi-index
-    in the shape of ["State","RegionName"].
-    
-    Note: Quarters are defined in the assignment description, they are
-    not arbitrary three month periods.
-    
-    The resulting dataframe should have 67 columns, and 10,730 rows.
-    '''
     df=pd.read_csv('City_Zhvi_AllHomes.csv')
     tdf=df.iloc[:,51:252]
     tdf=tdf.T.rename(lambda x: pd.to_datetime(x))
@@ -147,38 +99,21 @@ def convert_housing_data_to_quarters():
     out=out.set_index(["State","RegionName"])
     return out
 
-convert_housing_data_to_quarters()
 
 re_states={v:k for k,v in states.items()}
 def mapping(row):
     row['State']=re_states[row['State']]
     return row
-
 def get_bstart(df,r_start):
     df=df.T
     for i,index in enumerate(df.index):
         if index==r_start:
             pos=i
     return df.index[pos-1]
-
 def trending(row):
     row['change']=row[0]/row[2]
     return row
-
 def run_ttest():
-    '''First creates new data showing the decline or growth of housing prices
-    between the recession start and the recession bottom. Then runs a ttest
-    comparing the university town values to the non-university towns values, 
-    return whether the alternative hypothesis (that the two groups are the same)
-    is true or not as well as the p-value of the confidence. 
-    
-    Return the tuple (different, p, better) where different=True if the t-test is
-    True at a p<0.01 (we reject the null hypothesis), or different=False if 
-    otherwise (we cannot reject the null hypothesis). The variable p should
-    be equal to the exact p value returned from scipy.stats.ttest_ind(). The
-    value for better should be either "university town" or "non-university town"
-    depending on which has a lower mean price ratio (which is equivilent to a
-    reduced market loss).'''
     uni_towns=get_list_of_university_towns().apply(mapping, axis=1)
     housing_data=convert_housing_data_to_quarters()
     r_start=get_recession_start()
